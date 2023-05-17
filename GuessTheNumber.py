315225f46af77d305bfa46c9ff8ba57466852d4f
Anthony Baraja <atbaraja@gmail.com>
2023-05-16 20:29:43 -0400

    Add guess game

---
+++ GuessTheNumber.py
@@ -0,0 +1,36 @@
+# Game: This is a Guess the Number game via console.
+# Author: Anthony Baraja
+import random
+
+numOfGuesses = 6
+
+# Prompt user's name
+print('Hello! What is your name?')
+myName = input()
+
+number = random.randint(1, 20)
+print(f'Well, {myName}, I am thinking of a number between 1 and 20.')
+
+for guessesTaken in range(numOfGuesses):
+    print(f'\nYou have {numOfGuesses - guessesTaken} guesses remaining.')
+    print('Take a guess.')
+    guess = input()
+    guess = int(guess)
+
+    if guess < number:
+        print('Your guess is too low.')
+    
+    if guess > number:
+        print('Your guess is too high.')
+    
+    if guess == number:
+        print()
+        break
+
+if guess == number:
+    guessesTaken = str(guessesTaken + 1)
+    print(f'Good job, {myName}! You guessed my number in {guessesTaken} guesses!')
+
+if guess != number:
+    number = str(number)
+    print(f'Nope. The number I was thinking of was {number}.')
